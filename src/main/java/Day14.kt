8061bd2d7e0090e10a4cb2d2c08f240120d6f764
Anita Kosman <anitakosman95@gmail.com>
2020-12-17 12:31:52 +0100

    Day 14 part 2

--- src/main/java/Day14.kt
+++ src/main/java/Day14.kt
@@ -1,14 +1,13 @@
 import java.io.File
 
 fun main() {
-    val input = mutableMapOf<Int, Pair<String, String>>()
-    File("src/main/resources/day14.txt").readText().split(Regex("\\r\\n(?=mask)"))
-        .forEach { part ->
+    val input = File("src/main/resources/day14.txt").readText().split(Regex("\\r\\n(?=mask)"))
+        .flatMap { part ->
             val lines = part.lines()
             val mask = lines[0].substringAfter("= ")
-            lines.drop(1).forEach { line ->
+            lines.drop(1).map { line ->
                 Regex("mem\\[(\\d+)] = (\\d+)").matchEntire(line)!!.groups.let {
-                    input[it[1]!!.value.toInt()] = Pair(it[2]!!.value, mask)
+                    Instruction(it[1]!!.value.toInt(),it[2]!!.value.toInt(), mask)
                 }
             }
         }
@@ -16,14 +15,27 @@ fun main() {
     part2(input)
 }
 
-private fun part1(input: Map<Int, Pair<String, String>>) {
-    println(input.values.map { (value, mask) ->
-        val s = value.toInt().toString(2).padStart(36,'0').toCharArray()
+data class Instruction(val key: Int, val value: Int, val mask: String)
+
+private fun part1(input: List<Instruction>) {
+    println(input.reversed().distinctBy { it.key }.map { (_, value, mask) ->
+        val s = value.toString(2).padStart(36,'0').toCharArray()
         mask.forEachIndexed { i, c -> if (c == '1') s[i] = '1' else if (c == '0') s[i] = '0' }
         s.joinToString("").toLong(2)
     }.sum())
 }
 
-private fun part2(input: Map<Int, Pair<String, String>>) {
-
+private fun part2(input: List<Instruction>) {
+    val m = mutableMapOf<Long, Long>()
+    input.forEach { (key, value, mask) ->
+        var keys = setOf(StringBuilder(key.toString(2).padStart(36,'0')))
+        mask.forEachIndexed { index, c ->
+            when (c) {
+                '1' -> keys = keys.map { it.setCharAt(index, '1'); it }.toSet()
+                'X' -> keys = keys.flatMap { key -> listOf(StringBuilder(key).also { it.setCharAt(index, '0') }, StringBuilder(key).also { it.setCharAt(index, '1') } ) }.toSet()
+            }
+        }
+        keys.forEach { m[it.toString().toLong(2)] = value.toLong() }
+    }
+    println(m.values.sum())
 }
